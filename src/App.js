644b3605798aae5d211c62932927faa6373b3784
prakashvarma <prakashvarma4992@gmail.com>
2019-07-31 10:23:29 -0500

    New files
    Add files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';  
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="parent-box">
+            <div className="box-inner">
+                <p>BOX 1</p>
+            </div>
+            <div className="box-inner">
+                <p>BOX 2</p>
+            </div>
+            <div className="box-inner">
+                <p>BOX 3</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
